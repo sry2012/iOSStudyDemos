29fe8520e95174dc9aa7388eb2d0dc36731286db
sry2012 <sry2012@126.com>
2016-02-09 14:20:01 +0800

    color change

--- HelloWorld/HelloWorld/ViewController.m
+++ HelloWorld/HelloWorld/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.view.backgroundColor = [UIColor orangeColor];
+    self.view.backgroundColor = [UIColor redColor];
     
     //NSLog(@"Hello world");
     
